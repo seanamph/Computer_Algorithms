fd9166472c5e1d82725b9171a5a4660e39b60e59
seanamph <seanamph@hotmail.com>
2023-05-08 14:46:55 +0800

    Create insertsort.py

---
+++ chapter_3/insertsort.py
@@ -0,0 +1,16 @@
+def insert_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key
+
+arr = [4, 3, 2, 1, 5]
+insert_sort(arr)
+print(arr)
+
+
+
+
