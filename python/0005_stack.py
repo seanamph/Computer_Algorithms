05d5761874c752c6a534a8a69cd8cc43636391b3
seanamph <seanamph@hotmail.com>
2023-06-05 20:12:02 +0800

    Create 0005_stack.py

---
+++ python/0005_stack.py
@@ -0,0 +1,11 @@
+stack = []  # 創建一個空的堆疊
+
+stack.append(1)  # 將元素1壓入堆疊
+stack.append(2)  # 將元素2壓入堆疊
+stack.append(3)  # 將元素3壓入堆疊
+
+print(len(stack))  # 輸出堆疊中的元素數量
+
+stack.pop()  # 將堆疊頂部的元素彈出
+
+print(len(stack))  # 再次輸出堆疊中的元素數量
